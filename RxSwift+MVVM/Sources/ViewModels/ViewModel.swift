04f6485f7aa05e8d0aa89bd740216a396d7ca05b
getty <satoshi082015@gmail.com>
2017-03-16 01:06:47 +0900

    rename

---
+++ RxSwift+MVVM/Sources/ViewModels/ViewModel.swift
@@ -0,0 +1,24 @@
+//
+//  ViewModel.swift
+//  RxSwift+MVVM
+//
+//  Created by k-satoshi on 2017/03/13.
+//  Copyright © 2017年 k-satoshi. All rights reserved.
+//
+
+import UIKit
+
+class ViewModel: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
