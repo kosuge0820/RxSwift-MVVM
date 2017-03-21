87ba656f60a4eb98ec155eded43de36ab211efcf
getty <satoshi082015@gmail.com>
2017-03-21 17:28:01 +0900

    synx

---
+++ RxSwiftPractice/Sources/Examples/Numbers/NumbersViewController.swift
@@ -0,0 +1,34 @@
+//
+//  NumbersViewController.swift
+//  RxSwift+MVVM
+//
+//  Created by k-satoshi on 2017/03/13.
+//  Copyright © 2017年 k-satoshi. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+class NumbersViewController: UIViewController {
+
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    private let disposeBag = DisposeBag()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        combineLatest()
+    }
+    
+    private func combineLatest() {
+        Observable.combineLatest(textField1.rx.text.orEmpty, textField2.rx.text.orEmpty, textField3.rx.text.orEmpty) { value1, value2, value3 -> Int in
+                return (Int(value1) ?? 0) + (Int(value2) ?? 0) + (Int(value3) ?? 0)
+            }
+            .map { $0.description }
+            .bindTo { resultLabel.rx.text }
+            .disposed(by: disposeBag)
+    }
+}
