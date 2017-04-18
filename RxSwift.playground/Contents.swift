67e9ffa782ba0bb9d03cc9ebed8d2c7637911bdf
getty <satoshi082015@gmail.com>
2017-04-18 23:38:18 +0900

    added ignore function

--- RxSwift.playground/Contents.swift
+++ RxSwift.playground/Contents.swift
@@ -3,7 +3,6 @@
 ## RxSwift Practice
 
 */
-
 import UIKit
 import RxSwift
 import PlaygroundSupport
@@ -11,22 +10,18 @@ import PlaygroundSupport
 let one = 1
 let two = 2
 let three = 3
-
 /*:
 __just:__ 単一の要素
  */
-
 example(of: "just") {
     let observable = Observable<Int>.just(one)
     observable.subscribe(onNext: {
         print($0)
     })
 }
-
 /*:
 __of:__  定数のイベントストリームの生成
  */
-
 example(of: "of") {
     let observable = Observable.of(one, two, three)
     let observable1 = Observable.of([one, two, three])
@@ -39,34 +34,28 @@ example(of: "of") {
         print($0)
     })
 }
-
 /*:
 __from:__ 配列をObservableに変換
  */
-
 example(of: "from") { 
     let observable = Observable.from([one, two, three])
     observable.subscribe(onNext: {
         print($0)
     })
 }
-
 /*:
 __subscribe:__ イベントの登録
  */
-
 example(of: "subscribe") {
     let observable = Observable.of(one, two, three)
     observable.subscribe(onNext: {
         print($0)
     })
 }
-
 /*:
 __empty:__ 値を持たず、正常に終了するObservableを生成
 */
-
-example(of: "empty") { 
+example(of: "empty") {
     let observable = Observable<Void>.empty()
     observable.subscribe(
         onNext: {
@@ -76,8 +65,6 @@ example(of: "empty") {
         }
     )
 }
-
-
 /*:
 __never:__ 値を持たず、終了しないObservableを生成
  */
@@ -120,7 +107,7 @@ example(of: "dispose") {
     }
     subscription.dispose()
 }
-
+ 
 /*:
  __DisposeBag:__ データバインドの解除
  */
@@ -308,6 +295,42 @@ example(of: "Variable") {
     
     variable.value = "2"
 }
+
+example(of: "ignoreElements") { 
+    let strikes = PublishSubject<String>()
+    let disposeBag = DisposeBag()
+    
+    strikes
+        .ignoreElements()
+        .subscribe { _ in
+            print("You` re out!")
+        }
+        .disposed(by: disposeBag)
+    
+    strikes.onNext("X")
+    strikes.onNext("X")
+    strikes.onNext("X")
+    strikes.onCompleted()
+}
+
+example(of: "elementAt") { 
+    let strikes = PublishSubject<String>()
+    let disposeBag = DisposeBag()
+    
+    strikes
+        .elementAt(2)
+        .subscribe(onNext: { _ in
+            print("You 're out!")
+        })
+        .disposed(by: disposeBag)
+    
+    strikes.onNext("X")
+    strikes.onNext("X")
+    strikes.onNext("X")
+}
+
+
+
 
 
 
